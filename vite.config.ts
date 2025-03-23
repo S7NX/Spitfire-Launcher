573eb8b0252817f47b72c8d4e48b0651ae2615ac
BurakYs <brkyhs0@gmail.com>
2025-03-23 19:47:58 +0300

    Remove comments added by Tauri

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,9 @@ import path from 'node:path'
 
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [sveltekit(), tailwindcss()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -26,7 +20,6 @@ export default defineConfig(async () => ({
       }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ['**/src-tauri/**']
     },
     fs: {
