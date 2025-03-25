5308263f31f83afa994c0e51bdf73c606180a60b
BurakYs <brkyhs0@gmail.com>
2025-03-25 22:54:56 +0300

    Fix background and border colors not applying in StatusCard

--- src/lib/components/ui/StatusCard.svelte
+++ src/lib/components/ui/StatusCard.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-  import { cn } from '$lib/utils';
-  import defaultTheme from 'tailwindcss/defaultTheme';
-
-  const twColors = defaultTheme.colors();
-
   type Props = {
     title: string;
     message?: string;
-    color: keyof typeof twColors;
+    color: 'red' | 'green' | 'blue' | 'yellow' | 'indigo' | 'purple' | 'pink';
   };
 
   const { title, message, color }: Props = $props();
 </script>
 
-<div class="border rounded-lg p-3 mb-2 {cn(`bg-${color}-500/20 border-${color}-500/50`)}">
+<div class="border rounded-lg p-3 mb-2 bg-{color}-500/20 border-{color}-500/50">
   <div class="flex items-center gap-2">
-    <div class="size-3 rounded-full {cn(`bg-${color}-500`)}"></div>
+    <div class="size-3 rounded-full bg-{color}-500"></div>
     <span class="font-medium">{title}</span>
   </div>
 
