548ea28fc7c024b27cea5d1a338bd871f4dcf7a5
BurakYs <brkyhs0@gmail.com>
2025-04-12 22:17:23 +0300

    Implement a retry system for expired access tokens

--- src/lib/core/services/tauriKy.ts
+++ src/lib/core/services/tauriKy.ts
@@ -1,14 +1,26 @@
+import Authentication from '$lib/core/authentication';
+import { accessTokenCache, accountsStore } from '$lib/stores';
 import ky from 'ky';
 import { fetch } from '@tauri-apps/plugin-http';
 import Manifest from '$lib/core/manifest';
 import { isEpicApiError } from '$lib/utils/util';
 import EpicAPIError from '$lib/exceptions/EpicAPIError';
+import { get } from 'svelte/store';
+
+const MAX_AUTH_RETRIES = 1;
+
+type InternalProperties = {
+  _authRetryCount?: number;
+}
 
 // Used to avoid any CORS issues
 const tauriKy = ky.create({
-  fetch: async (input, options) => {
+  fetch: async (input, options: (RequestInit & InternalProperties) | undefined) => {
     const request = new Request(input, options);
     const headers = new Headers(request.headers);
+
+    options = options || {};
+    options._authRetryCount = options._authRetryCount || 0;
 
     if (!headers.has('x-user-agent')) {
       const userAgent = await Manifest.getUserAgent();
@@ -32,7 +44,45 @@ const tauriKy = ky.create({
       data = await response.text();
     }
 
-    if (isEpicApiError(data)) throw new EpicAPIError(data, request, response.status);
+    if (isEpicApiError(data)) {
+      const isInvalidTokenError = [
+        'errors.com.epicgames.common.authentication.token_verification_failed',
+        'errors.com.epicgames.common.oauth.invalid_token'
+      ].includes(data.errorCode);
+
+      if (isInvalidTokenError && options && options._authRetryCount < MAX_AUTH_RETRIES) {
+        options._authRetryCount++;
+
+        const accessTokenAccountId = Object.entries(get(accessTokenCache)).find(([, value]) => value.access_token === request.headers.get('Authorization')?.split(' ')[1])?.[0];
+        accessTokenCache.update(cache => {
+          if (accessTokenAccountId) {
+            delete cache[accessTokenAccountId];
+          }
+
+          return cache;
+        });
+
+        if (!accessTokenAccountId) {
+          throw new EpicAPIError(data, request, response.status);
+        }
+
+        const account = get(accountsStore).allAccounts.find(account => account.accountId === accessTokenAccountId)!;
+        const accessTokenData = await Authentication.getAccessTokenUsingDeviceAuth(account, false);
+
+        if (options.headers instanceof Headers) {
+          options.headers.set('Authorization', `Bearer ${accessTokenData.access_token}`);
+        } else {
+          options.headers = {
+            ...options.headers,
+            Authorization: `Bearer ${accessTokenData.access_token}`
+          };
+        }
+
+        return tauriKy(input, options);
+      } else {
+        throw new EpicAPIError(data, request, response.status);
+      }
+    }
 
     return new Response(data ? JSON.stringify(data) : null, response);
   },
