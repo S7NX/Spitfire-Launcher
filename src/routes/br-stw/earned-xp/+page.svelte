de7fadbebda45ba2cd5b2b923d16fcbc00e54e6c
BurakYs <brkyhs0@gmail.com>
2025-04-16 17:40:18 +0300

    Use system timezone, add translations

--- src/routes/br-stw/earned-xp/+page.svelte
+++ src/routes/br-stw/earned-xp/+page.svelte
@@ -11,29 +11,11 @@
 
   let isFetching = $state(false);
   let xpStatuses = $state<XPStatus[]>([]);
-
-  function getNextSunday(): Date {
-    const now = new Date();
-    const daysUntilSunday = 7 - now.getDay();
-    const nextSunday = new Date(now);
-    nextSunday.setDate(now.getDate() + (daysUntilSunday === 7 ? 0 : daysUntilSunday));
-    nextSunday.setHours(0, 0, 0, 0);
-    return nextSunday;
-  }
-
-  function getNextThursday(): Date {
-    const now = new Date();
-    const daysUntilThursday = 4 - now.getDay();
-    const nextThursday = new Date(now);
-    nextThursday.setDate(now.getDate() + (daysUntilThursday === 7 ? 0 : daysUntilThursday));
-    nextThursday.setHours(0, 0, 0, 0);
-    return nextThursday;
-  }
 </script>
 
 <script lang="ts">
   import CenteredPageContent from '$components/CenteredPageContent.svelte';
-  import { accountsStore, doingBulkOperations } from '$lib/stores';
+  import { accountsStore, doingBulkOperations, language } from '$lib/stores';
   import Button from '$components/ui/Button.svelte';
   import AccountCombobox from '$components/auth/account/AccountCombobox.svelte';
   import { getResolvedResults, t } from '$lib/utils/util';
@@ -47,9 +29,9 @@
     doingBulkOperations.set(true);
     xpStatuses = [];
 
-    const nextSunday = getNextSunday();
-    const nextThursday = getNextThursday();
-    
+    const nextSunday = getNextDayOfWeek(0);
+    const nextThursday = getNextDayOfWeek(4);
+
     const accounts = selectedAccounts.map((accountId) => $accountsStore.allAccounts.find((account) => account.accountId === accountId)).filter(x => !!x);
     await Promise.allSettled(accounts.map(async (account) => {
       const status = xpStatuses.find((status) => status.accountId === account.accountId) ||
@@ -83,6 +65,18 @@
     selectedAccounts = [];
     isFetching = false;
   }
+
+  function getNextDayOfWeek(dayIndex: number) {
+    const now = new Date();
+    const currentDay = now.getDay();
+    const daysUntilTarget = (7 + dayIndex - currentDay) % 7;
+
+    const nextDay = new Date();
+    nextDay.setUTCDate(now.getDate() + (daysUntilTarget === 0 ? 7 : daysUntilTarget));
+    nextDay.setUTCHours(0, 0, 0, 0);
+
+    return nextDay;
+  }
 </script>
 
 <CenteredPageContent
@@ -112,16 +106,19 @@
       {#snippet content(status)}
         {@const gamemodes = [
           {
+            id: 'battleRoyale',
             name: $t('common.gameModes.battleRoyale'),
             value: status.data.battleRoyale || 0,
             limit: 4_000_000
           },
           {
+            id: 'creative',
             name: $t('common.gameModes.creative'),
             value: status.data.creative || 0,
             limit: 4_000_000
           },
           {
+            id: 'saveTheWorld',
             name: $t('common.gameModes.saveTheWorld'),
             value: status.data.saveTheWorld || 0,
             limit: 4_000_000
@@ -129,7 +126,9 @@
         ]}
 
         <div class="bg-muted/30 p-3 space-y-6">
-          {#each gamemodes as gamemode (gamemode.name)}
+          {#each gamemodes as gamemode (gamemode.id)}
+            {@const resetDate = gamemode.id === 'saveTheWorld' ? status.data.stwResetDate : status.data.resetDate}
+
             <div class="space-y-1">
               <div class="flex items-center justify-between">
                 <div class="flex items-center gap-1.5">
@@ -149,21 +148,14 @@
 
               <div class="h-2 w-full bg-muted rounded-full overflow-hidden">
                 <div
-                  style="width: {Math.min(100,(gamemode.value / gamemode.limit) * 100)}%"
+                  style="width: {Math.min(100, (gamemode.value / gamemode.limit) * 100)}%"
                   class="h-full bg-epic"
                 ></div>
               </div>
-              {#if gamemode.name === $t('common.gameModes.saveTheWorld') && status.data.stwResetDate}
-                <div class="text-sm text-muted-foreground mt-1">
-                  Resets At: {status.data.stwResetDate.toLocaleDateString()}, 7:00 PM EST
-                </div>
-              {/if}
 
-              {#if gamemode.name !== $t('common.gameModes.saveTheWorld') && status.data.resetDate}
-                <div class="text-sm text-muted-foreground mt-1">
-                  Resets At: {status.data.resetDate.toLocaleDateString()}, 7:00 PM EST
-                </div>
-              {/if}
+              <div class="text-sm text-muted-foreground mt-1">
+                {$t('earnedXP.resetsAt', { time: resetDate.toLocaleString($language) })}
+              </div>
             </div>
           {/each}
         </div>
