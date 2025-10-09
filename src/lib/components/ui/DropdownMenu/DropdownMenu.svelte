368df4286c39c7588730b187832d43c88f8a6879
BurakYs <brkyhs0@gmail.com>
2025-10-09 20:19:41 +0300

    refactor: update dropdown open/close animations

--- src/lib/components/ui/DropdownMenu/DropdownMenu.svelte
+++ src/lib/components/ui/DropdownMenu/DropdownMenu.svelte
@@ -2,8 +2,6 @@
   import type { Snippet } from 'svelte';
   import { DropdownMenu, type WithoutChild } from 'bits-ui';
   import { cn } from '$lib/utils/util';
-  import { slide } from 'svelte/transition';
-  import { cubicInOut } from 'svelte/easing';
   import type { ClassValue } from 'svelte/elements';
 
   type Props = DropdownMenu.RootProps & {
@@ -41,20 +39,12 @@
       class={cn(
         'border-muted bg-background shadow-popover outline-hidden rounded-lg border p-1 z-50',
         'w-[var(--bits-dropdown-menu-anchor-width)] min-w-60 max-h-[var(--bits-dropdown-menu-content-available-height)] overflow-hidden',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--bits-dropdown-menu-content-transform-origin)',
         contentProps?.class
       )}
-      forceMount={true}
       sideOffset={8}
     >
-      {#snippet child({ wrapperProps, props, open })}
-        {#if open}
-          <div {...wrapperProps}>
-            <div {...props} transition:slide|local={{ duration: 150, easing: cubicInOut }}>
-              {@render children?.()}
-            </div>
-          </div>
-        {/if}
-      {/snippet}
+      {@render children?.()}
     </DropdownMenu.Content>
   </DropdownMenu.Portal>
 </DropdownMenu.Root>
